875a556ce5383a0b25d9b8ea51254d775b3e7e0f
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-08 14:20:55 -0300

    armado de rutas con DashboardRoutes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar } from "../components/ui/Navbar";
 import { LoginScreen } from "../components/login/LoginScreen";
-import { Marvel } from "../components/marvel/Marvel";
+import { DashboardRoutes } from "./DashboardRoutes";
+
 
 export const AppRouter = () => {
   return (
     <Router>
       <div>
-        <Navbar />
-
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
-          <Route exact path="/" component={Marvel} />
+          <Route path="/" component={ DashboardRoutes } />
         </Switch>
       </div>
     </Router>
