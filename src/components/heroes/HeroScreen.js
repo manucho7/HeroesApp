cde0f0076f9e5d4e88f3c333cb94306b4ce7a226
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-09 10:39:19 -0300

    Ruteando por params, para que si pasan heroe no existente, devuelva a pagina /

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,23 @@
 import React from 'react'
+import { useParams, Redirect } from 'react-router-dom'
+import { getHeroesById } from '../selectors/getHeroById';
 
 export const HeroScreen = () => {
+    const {heroeId} = useParams();
+    const hero = getHeroesById( heroeId );
+
+    if (!hero){
+        return <Redirect to="/" />
+    }
+
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+    } = hero;
+
     return (
         <div>
             <h1>HeroScreen</h1>
