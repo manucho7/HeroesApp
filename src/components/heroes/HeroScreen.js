2a3059cb85c00ed459c0fb0092d658cfd1da010c
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-09 14:25:35 -0300

    trabajando con HeroScreen para mostrar heroes por DC o Marvel

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,12 +2,22 @@ import React from 'react'
 import { useParams, Redirect } from 'react-router-dom'
 import { getHeroesById } from '../selectors/getHeroById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
     const {heroeId} = useParams();
     const hero = getHeroesById( heroeId );
 
     if (!hero){
         return <Redirect to="/" />
+    }
+
+    const handleReturn = () => {
+    
+        if ( history.length <= 2 ){
+            history.push("/");
+        } else {
+            history.goBack();
+        }
+
     }
 
     const {
@@ -19,8 +29,35 @@ export const HeroScreen = () => {
     } = hero;
 
     return (
-        <div>
-            <h1>HeroScreen</h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img 
+                    src={`../assets/${heroeId}.jpg`}
+                    className="img-thumbnail"
+                    alt={superhero}
+                />
+            </div>
+
+            <div className="col-8">
+                <h3>{ superhero }</h3>
+
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"> <b> Alter ego: </b> { alter_ego } </li>
+                    <li className="list-group-item"> <b> Publisher: </b> { publisher } </li>
+                    <li className="list-group-item"> <b> First appearance: </b> { first_appearance } </li>
+                </ul>
+
+                <h4>Characters</h4>
+                <p>{ characters }</p>
+
+                <button 
+                    className="btn btn-outline-info"
+                    onClick={ handleReturn }
+                >
+                    Return
+                </button>
+
+            </div>
         </div>
     )
 }
