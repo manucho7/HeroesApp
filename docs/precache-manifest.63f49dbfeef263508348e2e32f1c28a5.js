e0e5c5ec803454c86efe4fd03ab633c81c50c6c2
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-10 14:41:26 -0300

    Probando run build para hacer deploy

---
+++ docs/precache-manifest.63f49dbfeef263508348e2e32f1c28a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23cedcbd966fe5b6e55678e436a418d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9501e703fc4def6ebd5b",
+    "url": "/static/js/2.04ce1c3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.04ce1c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d2d06b71ca8b66800e0",
+    "url": "/static/js/main.17d405ec.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
