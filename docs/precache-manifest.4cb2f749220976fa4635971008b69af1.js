dcecfb2b3a25f10a5a1ae586c306a21c1182c540
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-10 10:19:39 -0300

    docs agregado

---
+++ docs/precache-manifest.4cb2f749220976fa4635971008b69af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ee28445bdfcf520b2ecacef50f17d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9501e703fc4def6ebd5b",
+    "url": "/static/js/2.04ce1c3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.04ce1c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "029329d57d1f4c075f1a",
+    "url": "/static/js/main.04bd204e.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
