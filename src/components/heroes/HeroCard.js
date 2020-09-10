cd41df57deab49688999c9b2d7c6517e23a007d6
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-10 15:14:23 -0300

    cambios para el deploy

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -13,7 +13,7 @@ export const HeroCard = ({
         <div className="card ms-3" style={ { maxWidth: 540 } }>
             <div className="row no-gutters">
                 <div className="col-md-4">
-                    <img src={`./assets/heroes/${id}.jpg`} className="card-img" alt={ superhero }/>
+                    <img src={`./assets/heroes/${id}.JPG`} className="card-img" alt={ superhero }/>
                 </div>
 
                 <div className="col-md-8">
