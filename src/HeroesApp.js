b62ef02039cd5d121f23c4710c38954a569c81af
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-10 17:59:27 -0300

    Generando AuthContext y authReducer para generar state default logged: false

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { AuthContext } from './auth/AuthContext'
 import { AppRouter } from './routers/AppRouter'
+import { authReducer } from './auth/authReducer';
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('user')) || { logged: false };
+}
 
 export const HeroesApp = () => {
+    const [user, dispatch] = useReducer(authReducer, {}, init)
+
     return (
         <div>
+            <AuthContext.Provider value={{ user, dispatch }}>
             <AppRouter />
+            </AuthContext.Provider>
         </div>
     )
 }
