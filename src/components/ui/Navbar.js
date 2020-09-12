7759346483ac525fa0736abb133c2a6ab11bbe89
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-12 10:28:00 -0300

    trabajando con Lougout via context, history y dispatch

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,11 +1,20 @@
-import React, { useContext } from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import { AuthContext } from '../../auth/AuthContext'
+import React, { useContext } from 'react';
+import { Link, NavLink, useHistory } from 'react-router-dom';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const Navbar = () => {
 
-    const { user: { name } } = useContext(AuthContext);
-    console.log(name);
+    const { user: { name }, dispatch } = useContext(AuthContext);
+    const history = useHistory();
+
+    const handleLogout = () => {
+        history.replace('/login');
+        dispatch({
+            type: types.logout
+        });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
@@ -55,14 +64,12 @@ export const Navbar = () => {
                         { name }
                     </span>
 
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
+                    <button 
+                        className="nav-item nav-link btn"
+                        onClick={ handleLogout }
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
