1616679ef1d92fff5016751cdf1888cd8a24064c
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-12 13:49:18 -0300

    Implementando propTypes para definir las rutas privadas de la app accesibles solo para usuarios verificados

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { AuthContext } from "../auth/AuthContext";
+import { PrivateRoute } from "./PrivateRoute";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
 
 
+
+
 export const AppRouter = () => {
+    const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
-          <Route path="/" component={ DashboardRoutes } />
+
+          <PrivateRoute 
+            path="/" 
+            component={ DashboardRoutes }
+            isAuthenticated={ user.logged }
+          />
         </Switch>
       </div>
     </Router>
