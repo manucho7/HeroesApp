707e3fa9282373841bc2bf7135260c3f502a02c5
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-13 10:53:31 -0300

    cambios en el LoginScreen para recordar ultima pagina

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -6,7 +6,7 @@ export const LoginScreen = ({ history }) => {
     const { dispatch } = useContext(AuthContext);
 
     const handleLogin = () => {
-        const lastPath = localStorage.getItem('lastPath') || '/'
+        const lastPath = localStorage.getItem('lastPath') || '/';
 
         dispatch({
             type: types.login,
