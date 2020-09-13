da49dce733338d6de44385914947b7a949b7389c
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-13 10:52:33 -0300

    Generando que la app recuerde la ultima pagina que quiso visitar el usuario previo al logout, y asi la muestre en el login

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -6,14 +6,15 @@ export const LoginScreen = ({ history }) => {
     const { dispatch } = useContext(AuthContext);
 
     const handleLogin = () => {
- //       history.push('/');
+        const lastPath = localStorage.getItem('lastPath') || '/'
+
         dispatch({
             type: types.login,
             payload: {
                 name: 'Manuchi'
             }
         });
-        history.replace('/');
+        history.replace( lastPath );
     }
 
     return (
