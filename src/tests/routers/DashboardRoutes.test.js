aa6cd767ba084b68f93409e49505b2c17a91bb5c
Manuel Blanco <manuel.blanco77@gmail.com>
2020-09-14 11:15:54 -0300

    Subiendo pruebas en <DashboardRoutes />

---
+++ src/tests/routers/DashboardRoutes.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { DashboardRoutes } from '../../routers/DashboardRoutes';
+import { AuthContext } from '../../auth/AuthContext';
+import '@testing-library/dom';
+import'@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+
+
+
+describe('Pruebas en <DashBoardRoutes />', () => {
+    const contextValue = {
+        dispatch: jest.fn(),
+        user: {
+            logged: true,
+            name: 'Manu'
+        }
+    }
+    
+    test('debe de mostrarse correctamente ', () => {
+        const wrapper = mount(
+            <AuthContext.Provider value={ contextValue }>
+                <MemoryRouter>
+                    <DashboardRoutes /> 
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('.text-info').text().trim() ).toBe('Manu');
+    });
+    
+
+})
